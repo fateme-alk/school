50cfdd10ca6b57773b7d1694935f0be821db88f4
fateme-alk <fateme.alk9@gmail.com>
2025-12-08 16:12:10 +0330

    add lesson model and related migrations

--- lessons/models.py
+++ lessons/models.py
@@ -1,1 +1,16 @@
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Lesson(models.Model):
+    title = models.CharField(
+        max_length=100,
+    )
+    classroom = models.PositiveSmallIntegerField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
