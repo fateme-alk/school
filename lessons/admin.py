de6d1d8f620fb52cfabcb66aaee48a7a141cf654
fateme-alk <fateme.alk9@gmail.com>
2025-12-13 15:22:38 +0330

    display list for lesson model in django web interface

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Lesson, Classroom
 
-admin.site.register(Lesson)
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+        "classroom",
+        "start_time",
+        "end_time",
+        "teacher",
+        "is_active",
+    ]
+
+
 admin.site.register(Classroom)
