da2ac2f9f3095d7243830c4a05e85459a57c9189
fateme-alk <fateme.alk9@gmail.com>
2025-12-13 13:51:18 +0330

    add classroom model and change relation with lesson model

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Lesson
+from .models import Lesson, Classroom
 
 admin.site.register(Lesson)
+admin.site.register(Classroom)
--- lessons/models.py
+++ lessons/models.py
@@ -5,11 +5,24 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Classroom(models.Model):
+    name = models.CharField(max_length=10)
+    is_usable = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if self.name:
+            self.name = self.name.capitalize()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name.capitalize()
+
+
 class Lesson(models.Model):
     title = models.CharField(
         max_length=100,
     )
-    classroom = models.PositiveSmallIntegerField()
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
     start_time = models.TimeField()
     end_time = models.TimeField()
     teacher = models.ForeignKey(User, on_delete=models.CASCADE)
