cead7ba7724605a0fa223f887b4b65b309fa19a0
fateme-alk <fateme.alk9@gmail.com>
2025-12-14 15:53:19 +0330

    write functional view for all lessons

--- lessons/views.py
+++ lessons/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from .models import Lesson
+from django.shortcuts import render
+
+
+def lesson_list(request):
+    all_lessons = Lesson.objects.all()
+    context = {
+        "all_lessons": all_lessons,
+    }
+    return render(request, "lessons/lesson_list.html", context=context)
