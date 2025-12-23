fccc1e386efad3893c3203582b1ef9f692e7a919
fateme-alk <fateme.alk9@gmail.com>
2025-12-23 11:20:00 +0330

    define active lessons view and map to specified url

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import lesson_list
+from .views import lesson_list, active_lessons
 
 urlpatterns = [
     path("all/", lesson_list),
+    path("active/", active_lessons),
 ]
--- lessons/views.py
+++ lessons/views.py
@@ -9,3 +9,11 @@ def lesson_list(request):
         "all_lessons": all_lessons,
     }
     return render(request, "lessons/lesson_list.html", context=context)
+
+
+def active_lessons(request):
+    active_lessons = Lesson.objects.filter(is_active=1)
+    context = {
+        "active_lessons": active_lessons,
+    }
+    return render(request, "lessons/active_lessons.html", context=context)
